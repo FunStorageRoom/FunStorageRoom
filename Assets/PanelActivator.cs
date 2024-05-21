44d42a8c7f16805b54b5ad2f4f9252519afee344
허재원 <jaewonheo666@gmail.com>
2024-05-21 23:47:47 +0900

    클릭해서 panel 여닫는 기능

--- Assets/PanelActivator.cs
+++ Assets/PanelActivator.cs
@@ -4,33 +4,29 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+
 public class PanelActivator : MonoBehaviour
 {
     public GameObject panel;
-    private XRGrabInteractable interactable;
+    private XRSimpleInteractable interactable;
 
     // Start is called before the first frame update
     void Start()
     {
-        interactable = GetComponent<XRGrabInteractable>();
+        interactable = GetComponent<XRSimpleInteractable>();
         interactable.selectEntered.AddListener(OnSelectEntered);
-        interactable.selectExited.AddListener(OnSelectExited);
-        
     }
+
     private void OnSelectEntered(SelectEnterEventArgs arg)
     {
         panel.SetActive(true);
     }
-    private void OnSelectExited(SelectExitEventArgs arg)
-    {
-        panel.SetActive(false);
 
-    }
     void OnDestroy()
     {
         interactable.selectEntered.RemoveListener(OnSelectEntered);
-        interactable.selectExited.RemoveListener(OnSelectExited);
     }
+
     // Update is called once per frame
     void Update()
     {
--- Assets/PopupDisabler.cs
+++ Assets/PopupDisabler.cs
@@ -1,13 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PopupDisabler : MonoBehaviour
 {
+    private XRSimpleInteractable interactable;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        interactable = GetComponent<XRSimpleInteractable>();
+        interactable.selectEntered.AddListener(OnSelectEntered);
+    }
+
+    private void OnSelectEntered(SelectEnterEventArgs arg)
+    {
+        gameObject.SetActive(false);
+    }
+    
+    void OnDestroy()
+    {
+        interactable.selectEntered.RemoveListener(OnSelectEntered);
     }
 
     // Update is called once per frame
