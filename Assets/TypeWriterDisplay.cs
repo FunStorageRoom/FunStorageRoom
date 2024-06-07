1c3aa675bf2ed2a50c74cd2f8989442cf9487d8c
허재원 <jaewonheo666@gmail.com>
2024-06-07 19:41:19 +0900

    Revert "Merge pull request #70 from FunStorageRoom/Feature/#67"
    This reverts commit 64bc3f3b9e4412c41aa0752986e17f0bc4c520f9, reversing
    changes made to b79c01285f7d09f92ca34efbb8ca8d72ef6af94c.

--- Assets/TypeWriterDisplay.cs
+++ Assets/TypeWriterDisplay.cs
@@ -4,14 +4,13 @@ using TMPro;
 public class TypewriterDisplay : MonoBehaviour
 {
     public static TypewriterDisplay Instance;
-    public TextMeshPro displayText; // 화면에 표시할 텍스트
+    public TextMeshProUGUI displayText; // 화면에 표시할 텍스트
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
@@ -21,12 +20,6 @@ public class TypewriterDisplay : MonoBehaviour
 
     public void TypeCharacter(string character)
     {
-        if(displayText != null)
-        {
-            displayText.text += character;
-        } else
-        {
-            Debug.LogWarning("displayText가 설정 안되었습니다.");
-        }
+        displayText.text += character;
     }
 }
